e849cfab96d021913336a16d031a4b5b0ad4866f
Artsiom Halian <fpm.galyanAA@bsu.by>
2024-11-11 17:10:17 +0300

    Update zero_grad.py

--- experiments/zero_grad.py
+++ experiments/zero_grad.py
@@ -82,7 +82,7 @@ def main(
             create_graph=True
         )
         
-        y_grad = torch.cat([g.view(-1) for g in y_grad])
+        y_grad = torch.cat([g.reshape(-1) for g in y_grad])
         loss = sum(torch.abs(y ** p).mean() for y in y_grad)
         loss.backward()
         optim.step()
